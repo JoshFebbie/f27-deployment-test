af66bc4230f699c3f91e6bdd57e6d886c83b0206
Josh Febbie <joshdfebbie@gmail.com>
2022-08-31 12:14:05 -0700

    js added

--- server/server.js
+++ server/server.js
@@ -5,14 +5,16 @@ const app = express();
 
 // Endpoints
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/index.html"))
+    res.sendFile(path.join(__dirname, "../client/index.html"));
 })
 
 app.get('/css', (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/styles.css"))
+    res.sendFile(path.join(__dirname, "../client/styles.css"));
 });
 
-
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/client.js"));
+});
 
 
 const port = process.env.PORT || 4005;
